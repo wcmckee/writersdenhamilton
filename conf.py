61674b019cfcd4fcdb077854cf71a5d0ade574fe
William Mckee <will@artcontrol.me>
2015-11-30 01:20:09 +1300

    end nano/theme redo

--- conf.py
+++ conf.py
@@ -272,7 +272,7 @@ COMPILERS = {
 # Nikola supports logo display.  If you have one, you can put the URL here.
 # Final output is <img src="LOGO_URL" id="logo" alt="BLOG_TITLE">.
 # The URL may be relative to the site root.
-LOGO_URL = 'writersdenhamilton.svg'
+#LOGO_URL = 'writersdenhamilton.svg'
 
 # If you want to hide the title of your website (for example, if your logo
 # already contains the text), set this to False.
@@ -971,7 +971,7 @@ MARKDOWN_EXTENSIONS = ['fenced_code', 'codehilite', 'extra']
 # RSS_LINK = None
 
 # Show teasers (instead of full posts) in feeds? Defaults to True.
-# RSS_TEASERS = True
+RSS_TEASERS = False
 
 # Strip HTML in the RSS feed? Default to False
 # RSS_PLAIN = False
